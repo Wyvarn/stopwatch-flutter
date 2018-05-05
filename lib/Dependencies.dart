a11d68bb59813ab68f9a4691f3afd307148cd89f
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-05-05 15:19:07 +0300

    init commit

---
+++ lib/Dependencies.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+import 'ElapsedTime.dart';
+
+class Dependencies {
+
+  final List<ValueChanged<ElapsedTime>> timerListeners = <ValueChanged<ElapsedTime>>[];
+  final TextStyle textStyle = const TextStyle(fontSize: 90.0, fontFamily: "Bebas Neue");
+  final Stopwatch stopwatch = new Stopwatch();
+  final int timerMillisecondsRefreshRate = 30;
+}
